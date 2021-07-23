177a2f0e4d19db1cb5a20e0061a848522819f054
NapoleonDLP <napoleon.dlp@gmail.com>
2021-07-23 13:42:33 -0700

    Added basic routes

--- server/app.js
+++ server/app.js
@@ -6,6 +6,27 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+//Questions
+  //Create a question
+  app.post('/questions', (req, res) => {
+
+  });
+  //Delete a question
+  app.delete('/questions', (req, res) => {
+
+  });
+  //Update a question
+  app.put('/questions', (req, res) => {
+
+  })
+
+//User
+  //Record result
+  app.post('/user', (req, res) => {
+
+  })
+
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
