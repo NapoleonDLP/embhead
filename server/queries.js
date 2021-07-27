6bcac3e3a8d4b62186b751cf60a9a16d8dfb64df
NapoleonDLP <napoleon.dlp@gmail.com>
2021-07-26 21:32:15 -0700

    Built getQuestions and postQuestion query

--- server/queries.js
+++ server/queries.js
@@ -1,9 +1,9 @@
-import pool from './db';
+const pool = require('./db.js');
 
 //list queries
 //get all questions
 const getQuestions = (req, res) => {
-  pool.query('SELECT * FROM questions', (error, results) => {
+  pool.pool.query('SELECT * FROM questions', (error, results) => {
     if (error) {
       throw error
     }
@@ -16,6 +16,19 @@ const getQuestions = (req, res) => {
 //delete question using ID
 
 //post a new question
+const postQuestion = (req, res) => {
+  const { question, answer, wrong_answers } = req.body;
+  pool.pool.query('INSERT INTO questions (question, answer, wrong_answers) VALUES ($1, $2, $3)', [question, answer, wrong_answers], (error, result) => {
+    if (error) {
+      throw error;
+    };
+
+    res.status(201).send(`Question added with ID:${result.insertId}`)
+  })
+};
 
 //put(update) a question
-
+module.exports = {
+  getQuestions,
+  postQuestion
+}
