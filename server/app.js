7f0fcc17a639073245ed4077398bcd01a6ec228c
NapoleonDLP <napoleon.dlp@gmail.com>
2021-07-26 21:29:48 -0700

    Added bodyParser dependency, Built out /questions get and /questions post

--- server/app.js
+++ server/app.js
@@ -1,34 +1,38 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
+const db = require("./queries.js");
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+
 });
 
 //Questions
-  //Get question by id
-  app.get('/question', (req, res) => {
+//Get question by id
+app.get('/question', (req, res) => {
+  db.getQuestions(req, res);
+});
+//Create a question
+app.post('/question', (req, res) => {
+  db.postQuestion(req, res);
+});
+//Delete a question
+app.delete('/question', (req, res) => {
 
-  });
-  //Create a question
-  app.post('/question', (req, res) => {
+});
+//Update a question
+app.put('/question', (req, res) => {
 
-  });
-  //Delete a question
-  app.delete('/question', (req, res) => {
-
-  });
-  //Update a question
-  app.put('/question', (req, res) => {
-
-  });
+});
 
 //User
-  //Record result
-  app.post('/user', (req, res) => {
+//Record result
+app.post('/user', (req, res) => {
 
-  });
+});
 
 
 app.listen(port, () => {
