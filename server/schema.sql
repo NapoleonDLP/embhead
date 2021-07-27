d4fbab8e3acb6b94c32e17934842339cda0b8636
NapoleonDLP <napoleon.dlp@gmail.com>
2021-07-26 21:28:03 -0700

    Changed table constraints

--- server/schema.sql
+++ server/schema.sql
@@ -7,7 +7,8 @@ CREATE DATABASE embhead;
 CREATE TABLE IF NOT EXISTS Questions (
   id SERIAL PRIMARY KEY,
   question text NOT NULL,
-  answer text NOT NULL
+  answer text NOT NULL,
+  wrong_answers text [3] NOT NULL
 );
 
 -- CREATE TABLE IF NOT EXISTS Users (
