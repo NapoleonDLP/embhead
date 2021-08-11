0b4609e00300844b4ac29c13926ae4e7b0fc2b4c
NapoleonDLP <napoleon.dlp@gmail.com>
2021-08-10 23:32:01 -0700

    Built out fetch functionality for card

--- client/src/Components/Card/index.jsx
+++ client/src/Components/Card/index.jsx
@@ -1,18 +1,18 @@
 import { useState, useEffect } from 'react';
 //Card will have the question and the possible answers
 
-export default function Card() {
+export default function Card(props) {
   // make a call to the server requesting the question based on question id
   // if no id is found request a random question
   const [wrongAnswers, setWrongAnswers] = useState(null);
   const [question, setQuestion] = useState(null);
-  const [id, setId] = useState(null); //should this be set to null or to id? if an id is present we would want this specific question to be retrieved. Or a random.
+  const [id, setId] = useState( props.id ); //should this be set to null or to id? if an id is present we would want this specific question to be retrieved. Or a random.
   const [answer, setAnswer] = useState(null);
 
   // make fetch request
   useEffect(() => {
-    const endpoint = (id === null) ? '/random' : `/:${id}`;
-    const url = `http://localhost:3001${endpoint}`;
+    const route = (id) ? `/question/${id}` : '/random';
+    const url = `http://localhost:3001${route}`;
 
     fetch(url, { credentials: 'omit' })
     .then(response => response.json())
