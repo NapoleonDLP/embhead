c8ec02d0d37506454f8a471697f773e051f06ae8
NapoleonDLP <napoleon.dlp@gmail.com>
2021-08-10 22:45:14 -0700

    Added cors dependencies and middleware

--- server/app.js
+++ server/app.js
@@ -3,8 +3,10 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 3001;
 const db = require("./queries.js");
+const cors = require('cors');
 
 app.use(bodyParser.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
 
