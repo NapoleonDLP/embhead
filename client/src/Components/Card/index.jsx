aa1cc7e958fed60e06d7f4c4f2f684d56c92a57e
NapoleonDLP <napoleon.dlp@gmail.com>
2021-08-10 23:04:34 -0700

    Built fetch for random card or id card

--- client/src/Components/Card/index.jsx
+++ client/src/Components/Card/index.jsx
@@ -1,16 +1,38 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 //Card will have the question and the possible answers
 
 export default function Card() {
   // make a call to the server requesting the question based on question id
   // if no id is found request a random question
+  const [wrongAnswers, setWrongAnswers] = useState(null);
   const [question, setQuestion] = useState(null);
+  const [id, setId] = useState(null); //should this be set to null or to id? if an id is present we would want this specific question to be retrieved. Or a random.
+  const [answer, setAnswer] = useState(null);
 
   // make fetch request
+  useEffect(() => {
+    const endpoint = (id === null) ? '/random' : `/:${id}`;
+    const url = `http://localhost:3001${endpoint}`;
+
+    fetch(url, { credentials: 'omit' })
+    .then(response => response.json())
+    .then(data => {
+      console.log('DATA:', data)
+      setWrongAnswers(data[0].wrong_answers);
+      setQuestion(data[0].question);
+      setAnswer(data[0].answer);
+      setId(data[0].id);
+    })
+    .catch((error) => {
+      console.error("ERROR:", error)
+    })
+
+
+  }, []);
 
   return (
     <div>
-      THIS IS THE QUESTION { question }
+      THIS IS THE QUESTION { question, answer, id, wrongAnswers }
     </div>
   )
 };
