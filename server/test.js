2ecd5616625958920d71db574e02c5d980d185fb
NapoleonDLP <napoleon.dlp@gmail.com>
2021-08-18 12:51:08 -0700

    Added GET /questions testing

--- server/test.js
+++ server/test.js
@@ -1,1 +1,18 @@
 const server = require('./app.js');
+const supertest = require('supertest');
+const requestWithSupertest = supertest(server);
+
+describe('User Endpoints', () => {
+
+  it('GET /questions should return all questions', async () => {
+    const res = await requestWithSupertest.get('/questions');
+
+    expect(res.status).toEqual(200);
+    expect(res.type).toEqual(expect.stringContaining('json'));
+    expect(res.body[0]).toHaveProperty('id');
+    expect(res.body[0]).toHaveProperty('question');
+    expect(res.body[0]).toHaveProperty('answer');
+    expect(res.body[0]).toHaveProperty('wrong_answers');
+
+  });
+});
